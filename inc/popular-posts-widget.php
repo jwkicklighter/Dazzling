b57570515d7949fdd8ce3451a7fd4bf064bd7c92
Jordan Kicklighter <jwkicklighter@gmail.com>
2015-03-13 00:10:48 -0400

    Modified popular posts widget to remove tabs.

--- inc/popular-posts-widget.php
+++ inc/popular-posts-widget.php
@@ -32,39 +32,13 @@ class dazzling_popular_posts_widget extends WP_Widget {
 
         <div class="widget tabbed">
             <div class="tabs-wrapper">
-                <ul class="nav nav-tabs">
-                      <li class="active"><a href="#popular-posts" data-toggle="tab"><?php esc_attr_e( 'Popular', 'dazzling' ); ?></a></li>
-                      <li><a href="#recent" data-toggle="tab"><?php esc_attr_e( 'Recent', 'dazzling' ); ?></a></li>
-                      <li><a href="#messages" data-toggle="tab"><i class="fa fa-comments tab-comment"></i></a></li>
-                </ul>
+                <!-- <ul class="nav nav-tabs"> -->
+                      <!-- <li class="active"><a href="#recent" data-toggle="tab"><?php esc_attr_e( 'Recent', 'dazzling' ); ?></a></li> -->
+                <!-- </ul> -->
 
             <div class="tab-content">
-                <ul id="popular-posts" class="tab-pane active">
-
-                    <?php
-                        $recent_posts = new WP_Query(array('showposts' => $number, 'ignore_sticky_posts' => 1, 'post_status' => 'publish', 'order'=> 'DESC', 'showposts' => $number, 'meta_key' => 'post_views_count', 'orderby' => 'meta_value'));
-                    ?>
-
-                    <?php while($recent_posts->have_posts()): $recent_posts->the_post(); ?>
-                        <li>
-                            <?php if ( has_post_thumbnail() ) : ?>
-                            <a href="<?php echo get_permalink() ?>" class="tab-thumb thumbnail" rel="bookmark" title="<?php the_title(); ?>">
-                                <?php the_post_thumbnail('tab-small'); ?>
-                            </a>
-                            <?php endif; ?>
-                            <div class="content">
-                                <a class="tab-entry" href="<?php echo get_permalink() ?>" rel="bookmark" title="<?php the_title(); ?>"><?php the_title(); ?></a>
-                                <i>
-                                    <?php the_time('M j, Y') ?>
-                                </i>
-                            </div>
-                        </li>
-                    <?php endwhile; ?>
-
-                </ul>
-                <?php wp_reset_postdata(); ?>
-
-                <ul id="recent" class="tab-pane">
+                <h2>Recent News</h2>
+                <ul id="recent" class="tab-pane active">
 
                     <?php
                     $recent_posts = new WP_Query(array('showposts' => $number,'post_status' => 'publish', 'ignore_sticky_posts' => 1 ));
@@ -88,34 +62,6 @@ class dazzling_popular_posts_widget extends WP_Widget {
                 </ul>
                 <?php wp_reset_postdata(); ?>
 
-                <ul id="messages" class="tab-pane">
-
-                <?php
-                $recent_comments = get_comments( array(
-                    'number'    => $number,
-                    'status'    => 'approve'
-                ) );
-                //var_dump($recent_comments);
-                ?>
-                <?php foreach($recent_comments as $comment) : ?>
-
-                    <li>
-                        <div class="content">
-                            <?php if ( $comment->comment_author ) { echo $comment->comment_author; } else { _e('Anonymous','dazzling'); } ?> <?php _e('on','dazzling'); ?>
-                            <a href="<?php echo get_permalink($comment->comment_post_ID) ?>" rel="bookmark" title="<?php echo get_the_title($comment->comment_post_ID); ?>">
-                                <?php echo get_the_title($comment->comment_post_ID); ?>
-                            </a>
-                            <p>
-                            <?php echo substr( $comment->comment_content, 0, strrpos( substr( $comment->comment_content, 0, 60), ' ' ) );?>
-                            <?php if (strlen($comment->comment_content) > 60 ) {echo '(...)'; } ?>
-                            </p>
-                        </div>
-                    </li>
-
-
-                 <?php endforeach; ?>
-
-                </ul>
                 </div>
             </div>
         </div>
