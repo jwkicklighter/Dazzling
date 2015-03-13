44e99b34bcb32dad862dccb4a52fea226b4c4386
Jordan Kicklighter <jwkicklighter@gmail.com>
2015-03-13 00:09:58 -0400

    Added calendar files to head.

--- header.php
+++ header.php
@@ -15,6 +15,45 @@
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
+<script src="//cdn.jsdelivr.net/jquery/2.1.3/jquery.min.js"></script>
+
+<!-- Calendar -->
+<script src="//cdnjs.cloudflare.com/ajax/libs/moment.js/2.9.0/moment.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.3.0/fullcalendar.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.3.0/gcal.js"></script>
+<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.3.0/fullcalendar.min.css" type="text/css">
+<link rel="print" href="//cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.3.0/fullcalendar.print.css" type="text/css">
+
+<script>
+	$(document).ready(function() {
+	  $('#calendar').fullCalendar({
+	    // THIS KEY WON'T WORK IN PRODUCTION!!!
+	    // To make your own Google API key, follow the directions here:
+	    // http://fullcalendar.io/docs/google_calendar/
+
+	    // Production Key:
+	    // googleCalendarApiKey: 'AIzaSyBaWrN6Qhm6bSYw7iypKur4sP0rprvsX9A',
+
+	    // Testing Key:
+	    googleCalendarApiKey: 'AIzaSyDcnW6WejpTOCffshGDDb4neIrXVUA1EAE',
+
+	    events: {
+	      url: 'https://www.google.com/calendar/feeds/kkyetasigma%40gmail.com/public/basic',
+	      className: 'gcal-event'
+	    },
+
+	    eventClick: function(event) {
+	      // opens events in a popup window
+	      window.open(event.url, 'gcalevent', 'width=700,height=600');
+	      return false;
+	    },
+
+	    loading: function(bool) {
+	      $('#loading').toggle(bool);
+	    }
+	  });
+	});
+</script>
 <?php wp_head(); ?>
 
 </head>
