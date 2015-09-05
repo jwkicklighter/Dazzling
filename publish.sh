9213d0107643b8b098240a8c070b4c00d1a87220
Jordan Kicklighter <jwkicklighter@yahoo.com>
2015-09-04 20:12:26 -0400

    Updated publish script to ignore files.

--- publish.sh
+++ publish.sh
@@ -3,5 +3,5 @@
 # Prepares theme for publishing
 
 rm Dazzling.zip
-zip -r Dazzling.zip ../
+zip -r Dazzling.zip * -x ".git" ".gitignore" "Gruntfile.js" "publish.sh" "Dazzling.sublime-project"
 echo "Theme zipped for publishing"
